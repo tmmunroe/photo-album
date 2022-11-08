ad274ced5daba804126f01c0407e8c28f042ff0d
Turner Mandeville <t.m.munroe@gmail.com>
2022-11-08 11:42:09 -0500

    fix constructor default for response model

--- photo_album_models/search_response.py
+++ photo_album_models/search_response.py
@@ -1,8 +1,8 @@
 from photo_album_models.photo_info import PhotoInfoModel
 
 class SearchResponseModel:
-    def __init__(self, photo_infos=list()):
-        self._photo_infos = photo_infos
+    def __init__(self, photo_infos=None):
+        self._photo_infos = photo_infos if photo_infos else []
     
     def add_photo_info(self, photo_info: PhotoInfoModel):
         self._photo_infos.append(photo_info)
