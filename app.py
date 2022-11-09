c01cecca403fbbe76bb26d5c527dc2c73aca543e
Turner Mandeville <t.m.munroe@gmail.com>
2022-11-09 17:18:29 -0500

    remove frontend code as it is in different repo; create frontend stack

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
 
 from stacks.backend_stack import PhotoAlbumStack
-from frontend.frontend_stack import PhotoAlbumFrontendStack
+from stacks.frontend_stack import PhotoAlbumFrontendStack
 
 
 env = cdk.Environment(account='756059218166', region='us-east-1')
@@ -12,6 +10,6 @@ env = cdk.Environment(account='756059218166', region='us-east-1')
 app = cdk.App()
 
 PhotoAlbumStack(app, "PhotoAlbumStack", env=env)
-# PhotoAlbumFrontendStack(app, "PhotoAlbumFrontend", env=env)
+PhotoAlbumFrontendStack(app, "PhotoAlbumFrontendStack", env=env)
 
 app.synth()
--- stacks/frontend_stack.py
+++ stacks/frontend_stack.py
@@ -2,7 +2,6 @@ import aws_cdk as cdk
 import aws_cdk.aws_s3 as s3
 
 from constructs import Construct
-from . import frontend
 
 class PhotoAlbumFrontendStack(cdk.Stack):
 
@@ -15,6 +14,11 @@ class PhotoAlbumFrontendStack(cdk.Stack):
             auto_delete_objects=True,
             website_index_document='index.html',
             website_error_document='index.html',
-            block_public_access=False,
             public_read_access=True,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False
+            )
         )
