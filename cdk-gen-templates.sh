e677819b8fb14a5e77742512418b6001388b34ba
Turner Mandeville <t.m.munroe@gmail.com>
2022-11-12 15:26:40 -0500

    add bash script to generate raw cloud formation templates for all stacks

---
+++ cdk-gen-templates.sh
@@ -0,0 +1,14 @@
+echo "Generating lambda layer..."
+./gen-lambda-layer.sh
+
+echo "Running 'cdk synth' for deployment stack"
+cdk synth PhotoAlbumDeploymentStack > templates/deployment.yml
+
+echo "Running 'cdk synth' for front end deployment stack"
+cdk synth PhotoAlbumDeploymentStack/DeploymentStage/PhotoAlbumFrontendDeploymentStack > templates/frontend-deployment.yml
+
+echo "Running 'cdk synth' for backend stack"
+cdk synth PhotoAlbumDeploymentStack/DeploymentStage/PhotoAlbumStack > templates/backend.yml
+
+echo "Running 'cdk synth' for frontend stack"
+cdk synth PhotoAlbumDeploymentStack/DeploymentStage/PhotoAlbumFrontendStack > templates/frontend.yml
